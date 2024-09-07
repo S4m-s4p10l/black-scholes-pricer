afbc19fe7a35aad1ea33e08e63410fa133129350
s4m-s4p10l <roman.kretov.20@ucl.ac.uk>
2024-09-07 16:56:14 +0100

    implement streamlit

---
+++ stream_app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+
+bs_tab, binomial_tab, mc_tab = st.tabs(["Black-Scholes model", "Binomial model", "Monte Carlo simulation"])
+
+#Black scholes tab
+
+
+#Binomial model tab
+
+
+
+
+#Monte Carlo simulation tab
+
